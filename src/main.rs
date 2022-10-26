51f965706b5884063361f080e573c336655668c7
BButner <butner.beau@gmail.com>
2022-10-26 00:19:47 -0500

    Rename

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use crate::lib::grpc::start_server;
 
 pub mod lib;
 
-pub static mut state: Option<Mutex<State>> = None;
+pub static mut STATE: Option<Mutex<State>> = None;
 
 #[tokio::main]
 async fn main() {
     unsafe {
-        state = Some(Mutex::new(State::new()));
+        STATE = Some(Mutex::new(State::new()));
     }
 
     start_server().await;
