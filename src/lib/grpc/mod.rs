cbf4f99a7ea972879e6931ed19d9a92f9ab81b02
BButner <butner.beau@gmail.com>
2022-10-26 08:55:40 -0500

    Change port

--- src/lib/grpc/mod.rs
+++ src/lib/grpc/mod.rs
@@ -3,7 +3,7 @@ use tonic::transport::Server;
 pub mod display_manager;
 
 pub async fn start_server() {
-    let addr = "0.0.0.0:50051".parse().unwrap();
+    let addr = "0.0.0.0:50052".parse().unwrap();
 
     Server::builder()
         .add_service(self::display_manager::get_service())
